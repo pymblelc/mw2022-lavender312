d026007b63e61c6d09af08d03ee5e9bba508e8d6
laven <ldavis@pymblelc.nsw.edu.au>
2022-06-21 11:33:13 +1000

    Css, bug fixes and incomplete elements

--- js/major.js
+++ js/major.js
@@ -1,7 +1,7 @@
 var api = '6237cbf0dced170e8c83a41d';
 var userUrl = 'https://ldavis-b83d.restdb.io/rest/majorlogins';
 var arrUsers = [''];
-var user = "Raymond";
+var loggedInUser = "";
 var arrUserIrritants = "lanolin";
 const AUTH0_CLIENT_ID = "XXK1MFFk3DwMidOeqZuKJvYxuXXEnDR0";
 const AUTH0_DOMAIN = "lavenmajor.au.auth0.com";
@@ -36,112 +36,6 @@ changePage("#btnScannerBack", "#scannerScreen", "#startPage");
 changePage("#btnUserBack", "#userHub", "#startPage");
 changePage('#btnSignup', "#loginPage", "#signUpPage");
 
-
-//login through authfication -- need to access connection between Auth0, restDB and Github 
-// Google and standalone authentication using Auth0 integration with restdb.io
-/*
-$(function () {
-    const AUTH0_CLIENT_ID = "XXK1MFFk3DwMidOeqZuKJvYxuXXEnDR0";
-    const AUTH0_DOMAIN = "lavenmajor.au.auth0.com";
-
-    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
-        auth: {
-            params: { scope: 'openid email profile' },
-            configurationBaseUrl: 'https://cdn.auth0.com',
-            responseType: 'token id_token'
-        }
-    });
-    // auto login
-    if (localStorage.getItem('id_token')) {
-        var token = localStorage.getItem('id_token');
-        lock.getProfile(token, function (error, profile) {
-            if (error) {
-                // Handle error
-                return;
-            }
-            // Display user information
-            show_profile_info(profile);
-
-            // global ajax Authorization setup
-            $.ajaxPrefilter(function (options) {
-                if (!options.beforeSend) {
-                    options.beforeSend = function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('id_token'));
-                    }
-                }
-            });
-
-            // get task items from database
-            getItems();
-        });
-    }
-    var show_profile_info = function (profile) {
-        console.log(profile.picture);
-        $('.nickname').text(profile.nickname);
-        $('.btn-login').hide();
-        $('.avatar').attr('src', profile.picture).show();
-        $('.btn-logout').show();
-    };
-
-    var retrieve_profile = function () {
-        var id_token = localStorage.getItem('id_token');
-        if (id_token) {
-            lock.getProfile(id_token, function (err, profile) {
-                if (err) {
-                    return alert('There was an error getting the profile: ' + err.message);
-                }
-                // Display user information
-                show_profile_info(profile);
-                // enable api button
-                $('.btn-api').removeAttr("disabled");
-            });
-        }
-    };
-
-    $('.btn-login').click(function (e) {
-        e.preventDefault();
-        lock.show();
-        return false;
-    });
-
-    $('.btn-logout').click(function (e) {
-        localStorage.removeItem('id_token');
-        $('.btn-api').attr("disabled", "true");
-        window.location.href = "/";
-        e.preventDefault();
-        return false;
-    });
-
-    lock.on("authenticated", function (authResult) {
-        lock.getProfile(authResult.accessToken, function (error, profile) {
-            if (error) {
-                // Handle error
-                console.log("Auth0 getProfile failed", error);
-                return;
-            }
-
-            localStorage.setItem('id_token', authResult.idToken);
-
-            // Display user information
-            show_profile_info(profile);
-
-            // global ajax Authorization setup
-            $.ajaxPrefilter(function (options) {
-                if (!options.beforeSend) {
-                    options.beforeSend = function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('id_token'));
-                    }
-                }
-            });
-
-            // get task items from database
-            getItems();
-        });
-    });
-});
-
-*/
-
 //Log in through Restdb
 
 var users;
@@ -163,12 +57,15 @@ $.ajax(settings).done(function (response) {
 
 //log in system
 function login(username, password) {
-    for (user of users){
-        if (user.email_address == username && user.Password == password){
-            console.log ("user yay")
+    for (user of users) {
+        if (user.email_address == username && user.Password == password) {
+            console.log(user);
+            loggedInUser = user;
             //success stuff here
             $("#loginPage").hide();
             $("#startPage").show();
+            document.getElementById("txtWelcome").innerText = "welcome back " + user.Name;
+            document.getElementById("profileName").innerText = user.Name;
             break;
         }
     }
@@ -179,9 +76,8 @@ $('#btnLogin').click(function () {
     );
 });
 
-//Sign up
-
-function addUser(item){
+//Sign up system
+function addUser(item) {
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -201,8 +97,7 @@ function addUser(item){
     });
 }
 
-
-$("#btnComplete").click(function() {
+$("#btnComplete").click(function () {
     let user = {
         Name: $("#signUpName")[0].value,
         email_address: $("#signUpUsername")[0].value,
@@ -211,6 +106,48 @@ $("#btnComplete").click(function() {
     addUser(user);
 });
 changePage("#btnComplete", "#signUpPage", "#loginPage")
+
+//user hub
+
+//irritants record
+function irritantLog() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://ldavis-b83d.restdb.io/rest/irritants",
+        "method": "GET",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": api,
+            "cache-control": "no-cache"
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        let thisUsersIrs = response.filter(function (v) {
+            if (v.Profile[0].email_address == loggedInUser.email_address) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+        document.getElementById("irritantList").innerHTML = "";
+        for (irritant of thisUsersIrs){
+            document.getElementById("irritantList").innerHTML += "<p>" + irritant.Ingredient[0]["INCI name"] + ;
+        }
+    });
+}
+
+$("#btnUserHub").click(function () {
+    irritantLog();
+});
+
+
+
+
+
+
+
 
 //Scanner
 /*function cameraActivate() {
@@ -227,6 +164,8 @@ changePage("#btnComplete", "#signUpPage", "#loginPage")
     };
 }
 cameraActivate();*/
+
+//ocr software (hella buggy)
 
 const player = document.getElementById('player')
 const snapshotZone = document.getElementById('snapshot')
@@ -252,8 +191,6 @@ captureButton.addEventListener('click', function () {
 //edit irritants
 
 /*$(function () {
-
-
     var errorMessages = {
         "REQUIRED": "This field is required",
         "UNIQUE": "This value already exists",
